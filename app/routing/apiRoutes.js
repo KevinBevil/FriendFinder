168ef8196ced0e39b0daa7550440bea3e526edd4
Kevin Bevil <kbevil@gmail.com>
2019-06-01 16:27:22 -0400

    Match logic finally seems to be working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,17 +1,44 @@
 var friendData = require("../data/friends");
 
-module.exports = function(app) {
+module.exports = function (app) {
    // API GET and POST requests
 
-   app.get("/api/friends", function(req, res) {
+   app.get("/api/friends", function (req, res) {
       res.json(friendData);
    });
 
-   app.post("/api/friends", function(req, res) {
+   app.post("/api/friends", function (req, res) {
       //Function to determine most appropriate friend by using the body's data
       // compared with friendData
+      var friendLength = friendData.length;
+      var yourScoreArray = [];
+      var quizLength = 10;
+      var diffTotal = 0;
+      var closestFit = 100;
 
-      console.log(req);
+      var bestFriend;
+      var bestFriendPic;
+
+      for (i = 0; i < quizLength; i++) {
+         yourScoreArray.push(req.body.scores[i]);
+      }
+
+
+      for (i = 0; i < friendLength; i++) {
+         diffTotal = 0;
+         for (j = 0; j < quizLength; j++) {
+            diffTotal += Math.abs(yourScoreArray[j] - friendData[i].scores[j]);
+            console.log(Math.abs(yourScoreArray[j] - friendData[i].scores[j]));
+         }
+         if (diffTotal < closestFit) {
+            closestFit = diffTotal;
+            bestFriend = friendData[i].name;
+            bestFriendPic = friendData[i].photo;
+         }
+      }
+
+      console.log("Your best friend is " + bestFriend + " and dude's picture is " + bestFriendPic);
+
    })
 }
 
